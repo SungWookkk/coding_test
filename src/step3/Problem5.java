059b99e4fbd192b4b45917093c1347c00d8c8d42
SungWookkk <patksy68@gmail.com>
2025-06-06 16:59:29 +0900

    6/6 feat: [단계별로 풀기] 3단계 반복문 6번 추가 - 7번부터 하기

--- src/step3/Problem5.java
+++ src/step3/Problem5.java
@@ -1,10 +1,37 @@
 package step3;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Problem5 {
     public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = br.readLine();
+
+        int a = Integer.parseInt(line);
+
+        StringBuilder sb =  new StringBuilder();
+        for (int i = 0; i < a; i++) {
+
+            String line1 = br.readLine();
+
+            StringTokenizer st = new StringTokenizer(line1);
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int sum = A + B;
+
+            sb.append(sum).append("\n");
+
+        }
+        System.out.println(sb.toString());
+
     }
+
 }
