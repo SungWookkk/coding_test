69734fe14872e52b1e1649fcfc3b914dcbaeb600
SungWookkk <patksy68@gmail.com>
2025-06-08 15:48:28 +0900

    6/8 feat:[시간복잡도] 시간복잡도 1~3번 문제 추가

---
+++ src/time_complexity/problem/Problem1.java
@@ -0,0 +1,14 @@
+package time_complexity.problem;
+
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
---
+++ src/time_complexity/problem/Problem3.java
@@ -0,0 +1,14 @@
+package time_complexity.problem;
+
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
