ab36bb1e4ce4ac5cf8ec1b228056b1099a465829
SungWookkk <patksy68@gmail.com>
2025-06-10 17:31:25 +0900

    6/10 feat: [단계별로 풀기] 3단계 반복문 10, 11번 추가

---
+++ src/step3/Problem10.java
@@ -0,0 +1,20 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if (x == 0 && y == 0) {
+                break;
+            }
+            System.out.println(x + y);
+
+        }
+    }
+}
