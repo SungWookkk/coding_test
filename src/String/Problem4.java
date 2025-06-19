542fa6adfd9e9a89e16cd0133ec3253d34cb92a0
SungWookkk <patksy68@gmail.com>
2025-06-19 17:36:21 +0900

    6/19 feat: [문자열] 문자열 4번 추가

---
+++ src/String/Problem4.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.util.Scanner;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+        int num = (int)ch;
+
+        System.out.println(num);
+    }
+}
