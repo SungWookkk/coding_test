8d1d317db928ae78b8b122de788cb2c0fe4e1a9c
SungWookkk <patksy68@gmail.com>
2025-06-19 17:28:13 +0900

    6/19 feat: [문자열] 문자열 2번 추가

---
+++ src/String/Problem2.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.nextLine();
+
+        sc.close();
+
+        int len = S.length();
+
+        System.out.println(len);
+    }
+}
