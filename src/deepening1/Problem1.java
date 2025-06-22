faa10b884d59c64abb35cc9bf5a47537fa3569a7
SungWookkk <patksy68@gmail.com>
2025-06-22 23:13:26 +0900

    6/22 style: [심화1] 심화 1번 추가

--- src/deepening1/Problem1.java
+++ src/deepening1/Problem1.java
@@ -4,7 +4,7 @@ public class Problem1 {
     public static void main(String[] args) {
         System.out.println("         ,r'\"7");
         System.out.println("r`-_   ,'  ,/");
-        System.out.println("\\. ".L_r'");
+        System.out.println("\\. L_r'");
         System.out.println("  `~\\/");
         System.out.println("     |");
         System.out.println("     |");
