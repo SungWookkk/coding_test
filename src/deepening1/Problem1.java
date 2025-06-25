cb62762052c05efc7b430685178adf990b267226
SungWookkk <patksy68@gmail.com>
2025-06-25 17:17:28 +0900

    6/25 style: [심화1] 심화 1번 틀렸던거 수정

--- src/deepening1/Problem1.java
+++ src/deepening1/Problem1.java
@@ -4,9 +4,9 @@ public class Problem1 {
     public static void main(String[] args) {
         System.out.println("         ,r'\"7");
         System.out.println("r`-_   ,'  ,/");
-        System.out.println("\\. L_r'");
-        System.out.println("  `~\\/");
-        System.out.println("     |");
-        System.out.println("     |");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
     }
 }
