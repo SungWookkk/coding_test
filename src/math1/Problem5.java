efc0ef710f9e3df50a35b599cf6c6df94885efc4
SungWookkk <patksy68@gmail.com>
2025-06-29 16:39:24 +0900

    6/29 feat: [수학1] 수학 5번 추가

---
+++ src/math1/Problem5.java
@@ -0,0 +1,26 @@
+package math1;
+
+import java.util.Scanner;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        int count = 1;
+        int end = 1;
+        int step = 6;
+
+        while (end <= n) {
+            end += step;
+            count++;
+            step += 6;
+        }
+        System.out.println(count);
+    }
+}
